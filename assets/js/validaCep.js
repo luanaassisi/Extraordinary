dd6837a34576b035e06d121499c65f5b9d8e1c56
luanaassisi <luanaaraujodeassis@outlook.com>
2022-06-07 18:19:48 -0300

    Estilo

--- assets/js/validaCep.js
+++ assets/js/validaCep.js
@@ -1,22 +1,30 @@
- $("#botao-cadastrar").on("click", function(event){
-   event.preventDefault()
-    let cep = $("#inputCep").val();
-    // let url = "https://viacep.com.br/ws/"+cep+"/json";
 
-$.ajax({
-    url: `https://viacep.com.br/ws/${cep}/json`, 
-    type:"GET",
-   
-    success: function(dados) {
+$("#botao-cadastrar").on("click", function (event) {
+  event.preventDefault()
+  let cep = $("#inputCep").val();
+  // let url = "https://viacep.com.br/ws/"+cep+"/json";
+
+  $.ajax({
+    url: `https://viacep.com.br/ws/${cep}/json`,
+    type: "GET",
+
+    success: function (dados) {
       console.log(dados);
-       $("#inputEstado").val(dados.uf);
-       $("#inputCidade").val(dados.localidade);
-       $("#inputBairro").val(dados.bairro);
-       $("#inputRua").val(dados.logradouro);
-      } 
-     })
+      $("#inputEstado").val(dados.uf);
+      $("#inputCidade").val(dados.localidade);
+      $("#inputBairro").val(dados.bairro);
+      $("#inputRua").val(dados.logradouro);
+    }
+  })
+
+  try { viaCEP() }
+
+  catch (error) {
+    console.log("CEP inválido")
+  }
 })
-//json.Stringly 
+
+//json.Stringly
 // function recuperarCEP(input) {
 //     const cep = input.value.replace(/\D/g, '');
 //     const url = `https://viacep.com.br/ws/${cep}/json`;
