17524f8266194a6beea1b5c5c8d7a586149ac5e8
Kamirii <camilyaalbres@gmail.com>
2022-06-07 13:43:48 -0300

    api conectada

---
+++ assets/js/validaCep.js
@@ -0,0 +1,43 @@
+ $("#botao-cadastrar").on("click", function(event){
+   event.preventDefault()
+    let cep = $("#inputCep").val();
+    // let url = "https://viacep.com.br/ws/"+cep+"/json";
+
+$.ajax({
+    url: `https://viacep.com.br/ws/${cep}/json`, 
+    type:"GET",
+   
+    success: function(dados) {
+      console.log(dados);
+       $("#inputEstado").val(dados.uf);
+       $("#inputCidade").val(dados.localidade);
+       $("#inputBairro").val(dados.bairro);
+       $("#inputRua").val(dados.logradouro);
+      } 
+     })
+})
+//json.Stringly 
+// function recuperarCEP(input) {
+//     const cep = input.value.replace(/\D/g, '');
+//     const url = `https://viacep.com.br/ws/${cep}/json`;
+//     const options = {
+//         method: "GET",
+//         mode: "cors",
+//         headers: {
+//             'content-type': 'application/json;charset=utf-8',
+//         }
+//     }
+//     if(!input.validity.patternMismatch && !input.validity.valueMissing) {
+//         fetch(url, options).then(
+//             response => response.json()
+//         ).then(
+//             data => {
+//                 console.log(data)
+//             }
+//         )
+//     }
+// }
+
+
+
+
